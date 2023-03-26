85be8291904c2d5124c54c820623ca60204c38e9
KANGHO <ariaskkh@gmail.com>
2023-03-26 21:06:18 +0900

    프로젝트 생성 + 폴더 및 파일 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './pages/recipes/recipe-detail/recipe-detail';
+import { RecipeItemComponent } from './pages/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './pages/recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { ShoppingEditComponent } from './pages/shopping-list/shopping-list-edit/shopping-edit';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/shopping-list/shopping-list-edit/shopping-edit.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-edit',
+  templateUrl: './shopping-edit.html',
+  styleUrls: ['./shopping-edit.css'],
+})
+export class ShoppingEditComponent {}
